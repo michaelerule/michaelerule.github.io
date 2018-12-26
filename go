58d6dcd2bb5ab6ceff520b85abf469fa6db97e41
M Rule <mrule7404@gmail.com>
2018-12-26 14:32:37 +0000

    revised auto-update script
    Former-commit-id: c586bea6d8ca1df7204c9e071af809f74856010f

--- go
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-./maketree.py
-git add --all
-git add . 
-git add -u :/
-git commit -m "updated website"
-git push -u origin master
---
+++ sync.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+./maketree.py
+
+# Clean up editor and temp files from the local directory (even if not 
+# tracked by git)
+echo "Deleting editor temporary files"
+find . -name "*.pyc" -exec rm -rf {} \; 2>/dev/null
+find . -name "*~" -exec rm -rf {} \;  2>/dev/null
+
+# Add any new files, add all updates to all files
+
+echo "Adding all changes"
+git add --all . 
+git add -u :/
+
+# Commit using the message specified as first argument to this script
+
+echo "Git commit"
+git commit -m "$1"
+
+# Synchronize with master on github
+echo "git pull"
+git pull
+
+echo "git push"
+git push origin master
